db454351572262a2a56c29add55cfc385f8f6a13
Lamina Samuel <samuellamina2@gmail.com>
2021-11-10 14:58:32 +0100

    api files added

---
+++ api/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./models/");
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// db
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "i don arrive." });
+});
+
+require("./routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
